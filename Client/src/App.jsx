434d9b6605d2b261311a24ca8189cbf42cb25970
Amel294 <amel294@gmail.com>
2024-02-06 17:21:04 +0530

    Routing done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,21 @@
-
-
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
 function App() {
 
   return (
-    <>
-        <h1 className="text-3xl">Hello world</h1>        
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
