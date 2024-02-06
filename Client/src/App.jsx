ee7dad1d297bc3eb3dfe40391efd714a56cdd4fb
Amel294 <amel294@gmail.com>
2024-02-06 17:32:18 +0530

    created header component and added link

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,10 +4,12 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
+import Header from './components/Header'
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
