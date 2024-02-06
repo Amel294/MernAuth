68326e82f3b6542b398899fdd1bfa82e3074ed56
Amel294 <amel294@gmail.com>
2024-02-06 18:06:14 +0530

    connected to mongo db Atlas

--- Api/index.js
+++ Api/index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
 
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected to MongoDB Atlas");
+}).catch((err)=>console.log(err))
 const app = express()
 
 
-app.listen(3000,()=>{
-    console.log(`Server running at localhost http://localhost:3000`)
+app.listen(PORT,()=>{
+    console.log(`Server running at localhost http://localhost:${PORT}`)
 })
