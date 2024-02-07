ef4eb013e1879694f78770dddb19c672d800244b
Amel294 <amel294@gmail.com>
2024-02-07 11:58:58 +0530

    create api sign in

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
 import UserModel from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
-
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     console.log(req.body)
-    const hashedPassword = bcryptjs.hashSync(password,10)
+    const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = UserModel({
         username,
         email,
-        password:hashedPassword
+        password: hashedPassword
     })
 
     try {
@@ -20,4 +21,27 @@ export const signup = async (req, res,next) => {
         next(error)
     }
 
+}
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+    console.log(req.body)
+    try {
+        const validUser = await UserModel.findOne({ email })
+        if (!validUser) return next(errorHandler(404, 'User not Found'))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials'))
+        const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET)
+    const {password:hashedPassword,...rest} = validUser._doc
+    const expiryDate = new Date(); // Create a new date object
+    expiryDate.setDate(expiryDate.getDate() + 7); // Set expiry to 7 days from now
+    
+    res.cookie('access_token', token, { 
+        httpOnly: true,
+        expires: expiryDate // Set the expiry date for the cookie
+    }).status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
 }
